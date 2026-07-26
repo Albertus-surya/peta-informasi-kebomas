8996176b80a703704d12c87b926723333854dec4
Albertus <albertussuryapangestu@gmail.com>
2026-07-26 13:06:38 +0700

    chore(map): perbarui URL API basemap

--- components/map/MapView.tsx
+++ components/map/MapView.tsx
@@ -109,8 +109,11 @@ export default function MapView({
         className="h-full w-full"
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Tiles &copy; Esri — Source: Esri'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+        maxZoom={19}
+        updateWhenIdle={false}
+        updateWhenZooming={true}
         />
         <BoundaryLayer
           data={boundaryData}
